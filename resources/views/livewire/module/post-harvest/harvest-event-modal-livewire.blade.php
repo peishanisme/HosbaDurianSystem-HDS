84c92d3941b2bb8b9b0cbdf5ebd434a02e5c5e3b
peishanisme <you@example.com>
2025-08-24 23:14:46 +0800

    change system logo / assign format for harvest event

--- resources/views/livewire/module/post-harvest/harvest-event-modal-livewire.blade.php
+++ resources/views/livewire/module/post-harvest/harvest-event-modal-livewire.blade.php
@@ -1,13 +1,15 @@
 <x-modal-component :id="$modalID" :title="$modalTitle">
-    <div class="fv-row mb-8">
-        <x-input-label for="name" class="required mb-2" :value="__('Event Name')" />
-        <x-input-text id="event_name" placeholder="Event Name" wire:model="form.event_name" />
-        <x-input-error :messages="$errors->get('form.event_name')" />
-    </div>
+    @if ($harvestEvent)
+        <div class="fv-row mb-8">
+            <x-input-label for="name" class="required mb-2" :value="__('Event Name')" />
+            <x-input-text id="event_name" placeholder="Event Name" wire:model="form.event_name" />
+            <x-input-error :messages="$errors->get('form.event_name')" />
+        </div>
+    @endif
 
     <div class="fv-row mb-8">
         <x-input-label for="name" class="required mb-2" :value="__('Start Date')" />
-        <x-input-text id="start_date" placeholder="Start Date" wire:model="form.start_date" type="date"/>
+        <x-input-text id="start_date" placeholder="Start Date" wire:model="form.start_date" type="date" />
         <x-input-error :messages="$errors->get('form.start_date')" />
     </div>
 
